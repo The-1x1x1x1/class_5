1b6f3f96aaf7849d4dd01212194beb914769b574
1x1x1x1 <nullandvoid186@gmail.com>
2025-09-15 18:36:01 +0100

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,7 @@
+num=int(input("pl34s3 3nt3r y0ur numb3r!! "))
+
+if num<15:
+    print("h1!!! 1m sm4ll3r th4n 15 4nd 1m 1n th3 1f bl0ck!!!")
+else:
+    print("H3Y PUNK! 1M B1GG3R TH4N 15 4ND 1N TH3 3LS3 BL0CK!!!")
+print("N0W 1M N0T 1N 31TH3R BL0CK!!!")
