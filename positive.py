e3e8fb0ed9ec39b59436b196ea370d5e8052c21e
1x1x1x1 <nullandvoid186@gmail.com>
2025-09-15 18:22:30 +0100

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,5 @@
+num=int(input("please enter a number: "))
+if num >=0:
+    print("w04h th1s numb3r 1s p0s1t1v3!!!!")
+else:
+    print("th1s numb3r 1s n3g4t1v3...")
---
+++ profitloss.py
@@ -0,0 +1,9 @@
+cp=float(input("pl3as3 3nt3r c0st pr1c3: "))
+sp=float(input("pl34s3 3nt3r s3ll1ng pr1c3: "))
+
+if sp>cp:
+    profit=sp-cp
+    print("w0w y0ur pr0f1t 1s: ", profit)
+else:
+    loss=cp-sp
+    print("4wwww y0ur l0ss 1s: ",loss)
